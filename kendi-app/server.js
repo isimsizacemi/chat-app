48e892f42f3c62236d02eb89c9ef247575d4c2ef
Abdurrahman Özkan <95485858+isimsizacemi@users.noreply.github.com>
2023-08-15 16:19:24 +0300

    Chat Projesi
    ilk hali

---
+++ kendi-app/server.js
@@ -0,0 +1,27 @@
+const { Socket } = require('dgram');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+io.on('connection' , (Socket)=>{
+    Socket.on('chat message', (msg) => {
+        io.emit('chat message', (msg));
+        console.log(`user : ${Socket.id} - message : ${msg}`)
+    });
+
+    Socket.on('disconnect',() => {
+        console.log("user siktirdi gitti");
+    });
+})
+  
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
